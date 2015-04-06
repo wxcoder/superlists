2c5f49bca6fdac117e1c23f6001aeb35b27eb266
wxcoder <car.mcnaught@gmail.com>
2015-04-06 02:20:37 -0400

    Front page HTML now generated from a template

--- lists/tests.py
+++ lists/tests.py
@@ -14,6 +14,6 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        expect_html = render_to_string('home.html')
+        expected_html = render_to_string('home.html')
         self.assertEqual(response.content.decode(), expected_html)
         
