f9ed2083fce2bef188c72bd771e1cbee507c768d
wxcoder <car.mcnaught@gmail.com>
2015-04-18 02:53:50 -0400

    use form in all views, back to working state

--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,5 @@
-from django.core.exceptions import ValidationError
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+
 from lists.forms import ItemForm
 from lists.models import Item, List
 
@@ -8,29 +7,21 @@ def home_page(request):
     return render(request, 'home.html', {'form': ItemForm()})
 
 def view_list(request, list_id):
-    list_ = List.objects.get(id = list_id)
-    error = None
-
+    list_ = List.objects.get(id=list_id)
+    form = ItemForm()
     if request.method == 'POST':
-        try:
-            item = Item(text=request.POST['text'], list=list_)
-            item.full_clean()
-            item.save()
-            return redirect('/lists/%d/' % (list_.id,))
-        except ValidationError:
-            error = "You can't have an empty list item"
-
-    return render(request, 'list.html', {'list': list_, 'error': error})
+        form = ItemForm(data=request.POST)
+        if form.is_valid():
+            Item.objects.create(text=request.POST['text'], list=list_)
+            return redirect(list_)
+    return render(request, 'list.html', {'list': list_, "form": form})
 
 def new_list(request):
-    list_ = List.objects.create()
-    item = Item(text=request.POST['text'], list=list_) #takes item from POST in html file
-    try:
-        item.full_clean()
-        item.save()
-    except ValidationError:
-        list_.delete()
-        error = "You can't have an empty list item"
-        return render(request, 'home.html', {"error": error})
-    return redirect(list_)
+    form = ItemForm(data=request.POST) #pass request.POST data into the form's constructor
+    if form.is_valid(): # determine whether the submission is good or bad
+        list_ = List.objects.create()
+        Item.objects.create(text=request.POST['text'], list=list_)
+        return redirect(list_)
+    else:
+        return render(request, 'home.html', {"form": form}) # pass the form down the template
  
