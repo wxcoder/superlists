d40cc561e79a5e145b0579a8f885102f8d2c3b35
wxcoder <car.mcnaught@gmail.com>
2015-04-19 03:03:36 -0400

    Using Existing List Item Form in the List View

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -2,8 +2,11 @@ from django.utils.html import escape
 from django.test import TestCase
 
 from lists.models import Item, List
-from lists.forms import ItemForm, EMPTY_ITEM_ERROR
-from unittest import skip
+from lists.forms import (
+    ItemForm, ExistingListItemForm, 
+    EMPTY_ITEM_ERROR, DUPLICATE_ITEM_ERROR
+)
+
 
 class HomePageTest(TestCase):
     maxDiff = None
@@ -87,13 +90,13 @@ class ListViewTest(TestCase):
         
     def test_for_invalid_input_passes_form_to_template(self):
         response = self.post_invalid_input()
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
 
     def test_for_invalid_input_shows_error_on_page(self):
         response = self.post_invalid_input()
         self.assertContains(response, escape(EMPTY_ITEM_ERROR))
+
         
-    @skip    
     def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
         list1 = List.objects.create()
         item1 = Item.objects.create(list=list1, text='textey')
@@ -102,15 +105,15 @@ class ListViewTest(TestCase):
             data={'text': 'textey'}
         )
 
-        expected_error = escape("You've already got this in your list")
+        expected_error = escape(DUPLICATE_ITEM_ERROR)
         self.assertContains(response, expected_error)
         self.assertTemplateUsed(response, 'list.html')
-        self.assertEqual(Item.objects.all().count, 1)
+        self.assertEqual(Item.objects.all().count(), 1)
 
     def test_displays_item_form(self):
         list_ = List.objects.create()
         response = self.client.get('/lists/%d/' % (list_.id,))
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
         self.assertContains(response, 'name="text"')
 
 class NewItemTest(TestCase):
