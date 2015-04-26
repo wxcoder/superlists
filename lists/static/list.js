fff4a0b80ffce38407d63d82a2df3cf26486d3e6
wxcoder <car.mcnaught@gmail.com>
2015-04-26 00:54:36 -0400

    update lists directory

---
+++ lists/static/list.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function ($) {
+	$('input[name="text"]').on('keypress', function() {
+    	$('.has-error').hide();
+	});
+
+	$('input[name="text"]').on('click', function() {
+		$('.has-error').hide();
+
+	});
+});
