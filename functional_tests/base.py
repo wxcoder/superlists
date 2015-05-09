aa897c489e53c13332045aa8e41fc04d4cf73e72
wxcoder <car.mcnaught@gmail.com>
2015-05-09 03:38:59 -0400

    use wait_for function for URL checks in my_lists

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -5,19 +5,15 @@ import os
 from datetime import datetime
 import sys
 from .server_tools import reset_database
-from pyvirtualdisplay import Display
-from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+import time
+from selenium.common.exceptions import WebDriverException
+
 #from .server_tools import reset_database
 
-
+DEFAULT_WAIT = 5
 SCREEN_DUMP_LOCATION = os.path.join(
     os.path.dirname(os.path.abspath(__file__)), 'screendumps'
 )
-
-
-display = Display(visible=0, size=(1024, 768))
-display.start()
-binary = FirefoxBinary('/usr/local/firefox/firefox')
 
 class FunctionalTest(StaticLiveServerTestCase):
 
@@ -43,8 +39,8 @@ class FunctionalTest(StaticLiveServerTestCase):
         """Open a Firefox  webpage"""
         if self.against_staging:
             reset_database(self.server_host)
-        self.browser = webdriver.Firefox(firefox_binary=binary)
-        self.browser.implicitly_wait(3)
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(DEFAULT_WAIT)
 
     def tearDown(self):
         if self._test_has_failed():
@@ -113,4 +109,14 @@ class FunctionalTest(StaticLiveServerTestCase):
         self.wait_for_element_with_id('id_login')
         navbar = self.browser.find_element_by_css_selector('.navbar')
         self.assertNotIn(email, navbar.text)
+
+    def wait_for(self, function_with_assertion, timeout=DEFAULT_WAIT):
+        start_time = time.time()
+        while time.time() - start_time < timeout:
+            try:
+                return function_with_assertion()
+            except (AssertionError, WebDriverException):
+                time.sleep(0.1)
+        # one more try, which will raise any errors if they are outstanding
+        return function_with_assertion()
 
