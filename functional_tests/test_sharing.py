05ff6487bfd520709475b5a5eb3182e78ae9bd67
wxcoder <car.mcnaught@gmail.com>
2015-05-09 20:56:19 -0400

    Create Page objects for Home and List pages, use in sharing FT

--- functional_tests/test_sharing.py
+++ functional_tests/test_sharing.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from .base import FunctionalTest
+from .home_and_list_pages import HomePage
 
 def quit_if_possible(browser):
 	try: browser.quit()
@@ -20,12 +21,23 @@ class SharingTest(FunctionalTest):
 
 		# Edith goes to the home page and starts a list
 		self.browser = edith_browser
-		self.browser.get(self.server_url)
-		self.get_item_input_box().send_keys('Get help\n')
+		list_page = HomePage(self).start_new_list('Get help')
 
 		# She notices a "Share this list" option
-		share_box = self.browser.find_element_by_css_selector('input[name=email]')
+		share_box = list_page.get_share_box()
 		self.assertEqual(
 			share_box.get_attribute('placeholder'),
 			'your-friend@example.com'
 		)
+
+		# She shares her list.
+		# The pages updates to say that it's shared with Oniciferous:
+		list_page.share_list_with('oniciferous@example.com')
+
+		# Oniciferous now goes to the lists page with his browser
+		self.browser = oni_browser
+		HomePage(self).go_to_home_page().go_to_my_lists_page()
+
+		# He sees Edith's list in there!
+		self.browser.find_element_by_link_text('Get help').click()
+		
